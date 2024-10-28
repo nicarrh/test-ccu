db79c538236e27f29ae202b8d0b5cbac69c397a0
Nico C <nicolas.carreno@coderslab.io>
2024-10-28 19:49:07 -0300

    feat: (constants) some consts added

---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,13 @@
+const darkBlue = '#060009';
+const midBlue = '#232931';
+const softBlue = '#111418';
+const white = '#FFFFFF';
+const red = '#FF0000';
+
+export default {
+  darkBlue,
+  midBlue,
+  softBlue,
+  white,
+  red,
+};
