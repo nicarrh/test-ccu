ededf68818b8c0ee3bb5cb0a5175ea8627723c96
Nico C <nicolas.carreno@coderslab.io>
2024-10-28 19:55:08 -0300

    feat: (shared) export modules from shared

---
+++ src/shared/index.ts
@@ -0,0 +1,16 @@
+import * as RequestHelper from './helpers/RequestHelper';
+import ErrorLoger from './helpers/ErrorLoger';
+import SecureStorage from './helpers/SecureStorage';
+import Validations from './helpers/Validations';
+import Constants from './constants';
+import Layout from './components/Layout';
+import { RootStackParamList } from './types/Navigation';
+export {
+  RequestHelper,
+  SecureStorage,
+  ErrorLoger,
+  Constants,
+  Validations,
+  Layout,
+  RootStackParamList,
+};
